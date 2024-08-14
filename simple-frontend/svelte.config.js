b69e31e9a0fece7293207a289fd8be97bda124d3
Daniel Koch <daniel.koch@helsinki.fi>
2024-08-14 13:22:31 +0300

    remove old asset path, app is running in a different environment now

--- simple-frontend/svelte.config.js
+++ simple-frontend/svelte.config.js
@@ -13,7 +13,6 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			assets: 'http://4.225.90.153/ai-creator',
 		}
 	},
 };
