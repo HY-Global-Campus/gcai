89d6a80b0e9804f2588f7f12167a72a0ddcce653
Daniel Koch <daniel.koch@helsinki.fi>
2025-06-03 17:21:10 +0300

    undo previous changes, but change record_id to serde rename recordId

--- backend/src/skills/merge_pdf_metadata/types.rs
+++ backend/src/skills/merge_pdf_metadata/types.rs
@@ -2,34 +2,35 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 pub struct SkillRequest {
-    values: Vec<Record>,
+    pub values: Vec<Record>,
 }
 
 #[derive(Deserialize)]
 pub struct Record {
-    record_id: Option<String>,
-    data: Option<RecordData>,
+    #[serde(rename = "recordId")]
+    pub record_id: String,
+    pub data: RecordData,
 }
 
 #[derive(Deserialize)]
 pub struct RecordData {
-    metadata_title: Option<String>,
-    metadata_author: Option<String>,
-    content: Option<String>,
+    pub metadata_title: Option<String>,
+    pub metadata_author: Option<String>,
+    pub content: Option<String>,
 }
 
 #[derive(Serialize)]
 pub struct SkillResponse {
-    values: Vec<SkillResponseRecord>,
+    pub values: Vec<SkillResponseRecord>,
 }
 
 #[derive(Serialize)]
 pub struct SkillResponseRecord {
-    record_id: String,
-    data: SkillResponseData,
+    pub record_id: String,
+    pub data: SkillResponseData,
 }
 
 #[derive(Serialize)]
 pub struct SkillResponseData {
-    merged_content: String,
+    pub merged_content: String,
 }
